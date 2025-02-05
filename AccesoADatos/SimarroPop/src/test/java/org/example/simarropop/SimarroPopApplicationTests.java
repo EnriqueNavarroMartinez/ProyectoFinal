ad61ad375275215acc57ccbc0cb36cc64d1105e3
emeteme <crazydraco24@outlook.com>
2025-02-05 13:29:20 +0100

    Esto va a petar

---
+++ AccesoADatos/SimarroPop/src/test/java/org/example/simarropop/SimarroPopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.simarropop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimarroPopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
